7aa1f6ebb699d43e2c65416f56fc0613e5524e39
Luis Fernando Gutiérrez Romo <changoluis@hotmail.com>
2016-09-10 13:35:19 -0500

    Create db.sql

---
+++ database/db.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE whereAreU;
+USE whereAreU;
+
+
+CREATE TABLE Denuncia(
+Fecha datetime,
+Name VARCHAR(50),
+Image VARCHAR(150)
+);
+
+CREATE TABLE Pictures(
+Fecha datetime,
+Image VARCHAR(150)
+);
