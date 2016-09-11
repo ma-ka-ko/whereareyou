02d918856590767a180d30ec0ef30ef00e728fc3
Luis Fernando Gutiérrez Romo <changoluis@hotmail.com>
2016-09-11 11:44:06 -0500

    Add files via upload

---
+++ database/dbscript.ps1
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# MySQL Workbench Python script
+# <description>
+# Written in MySQL Workbench 6.3.7
+
+import grt
+#import mforms
+sudo tee /etc/sudoers.d/$USER <<END
+END
+
+# write the content of your script here
+sudo npm install hexo-cli -g
+mkdir Untitled
+sudo apt-get install mysql
+$ mysql -u root 
+mysql> CREATE DATABASE wereareyou;
+mysql> USE wereareyou;
+mysql> CREATE TABLE Denuncia(Fecha datetime, Name VARCHAR(50), Image VARCHAR(150));
+mysql> CREATE TABLE Pictures(Fecha datetime, Image VARCHAR(150));
+sudo apt-get install python
+
+
+# then to remove the sudo access from the current user
+sudo /bin/rm /etc/sudoers.d/$USER
+sudo -k
